d249f631d48bce9a7dbca9bc3cb4a8b0449103ff
Maria Tatarkina <sirius_4752@outlook.com>
2019-02-03 21:55:25 +0400

    Changes (lesson 11)
    Добавлено следующее:
    1) слайдер;
    2) фильтрация и сортировка;
    3) функция для кнопки "назад";
    4) работа с корзиной
    - добавление элемента в корзину;
    - очистка корзины;
    - увеличение/уменьшение количества элементов в корзине.

--- js/phones/components/filter.js
+++ js/phones/components/filter.js
@@ -1,20 +1,88 @@
-export default class Filter {
-  constructor({ element }) {
+export default class Filter{
+  constructor({ element, phones, catalog }){
     this._element = element;
-
+    this._catalog = catalog;
     this._render();
+    this._phones = phones;
+
+
+    this._positiveArr = []
+    this._element.querySelector('[data-element = "search"]').addEventListener('keyup', (event) => {
+      this.search(event.target.value.toLowerCase());
+    });
+    this._element.querySelector('[data-select]').addEventListener('change', (event) => {
+      this.filter(event.target.value);
+    });
+  }
+  search(elem){
+     this._positiveArr = this._phones.filter(function(item) {
+      let itemName = item.name.toLowerCase();
+      if (itemName.indexOf(elem) == -1 ) return false;
+      return item; 
+    });
+    this._renderCatalog(this._positiveArr)
+  }
+  filter(elem){
+    let arr = this._phones;
+    console.log(elem);
+
+    function compareNumeric(a, b) {
+      if (a[elem] > b[elem]) return 1;
+      if (a[elem] < b[elem]) return -1;
+    }
+    arr.sort(compareNumeric);
+    this._renderCatalog(arr);
+  }
+  _renderCatalog(newArr){
+    this._catalog.innerHTML = `
+        <ul class="phones">
+          ${  newArr.map((elem) => `
+              <li 
+                data-element="phone"
+                data-phone-id="${ elem.id }"
+                class="thumbnail"
+              >
+                <a 
+                  data-element="phone-link"
+                  href="#!/phones/${ elem.id }" 
+                  class="thumb" 
+                >
+                  <img alt="${ elem.name }™ with Wi-Fi" src="${ elem.imageUrl }">
+                </a>
+
+                <div class="phones__btn-buy-wrapper">
+                <a 
+                  data-add-to-cart = "${ elem.id }"
+                  class="btn btn-success">
+                  Add
+                </a>
+              </div>
+
+              <a 
+                data-element="phone-link"
+                href="#!/phones/${ elem.id }"  
+              >
+                ${ elem.name }
+              </a>
+              <p>
+                ${ elem.snippet }
+              </p>
+              </li>
+              `).join('')
+            }
+         </ul>
+    `;
   }
 
-  _render() {
-    this._element.innerHTML = `
+  _render(){
+    this._element.innerHTML = `          
       <p>
         Search:
-        <input>
+        <input data-element = 'search' value=''>
       </p>
-
       <p>
         Sort by:
-        <select>
+        <select data-select>
           <option value="name">Alphabetical</option>
           <option value="age">Newest</option>
         </select>
--- js/phones/components/shopping-cart.js
+++ js/phones/components/shopping-cart.js
@@ -1,18 +1,76 @@
-export default class ShoppingCart {
-  constructor({ element }) {
+export default class ShoppingCart{
+  constructor({ element }){
     this._element = element;
 
+    this._insideCart = [];
+    this.find = function(id){
+      for (var i = 0; i < this._insideCart.length; i++){
+        if (this._insideCart[i][0] === id) return i;
+      }
+      return false;
+    }
+    this._render();
+    this._element.addEventListener('click', (event) => {
+      if (event.target.dataset.element == 'clean-cart') {
+        this.removeAll();
+        return;
+      }
+      let thisId = event.target.closest('li[data-id]').dataset.id;
+      if (event.target.dataset.element == 'add-button'){
+        this.add(thisId);
+      }
+      if (event.target.dataset.element == 'delete-button'){
+        this.delete(thisId);
+        console.log(this._insideCart );
+      }
+      if (event.target.dataset.element == 'remove-button') {
+        this.remove(thisId);
+      }
+
+    });
+  }
+  add(phoneId){
+    let newElem = [phoneId, 1];
+    let idFounded = this.find(phoneId);
+    if (idFounded === false) this._insideCart.push(newElem);
+    else this._insideCart[idFounded][1]++;
+    this._render();
+  }
+  delete(phoneId){
+    let idFounded = this.find(phoneId);
+    if(this._insideCart[idFounded][1] > 1 ) this._insideCart[idFounded][1]--;
+    else this.remove(phoneId);
+    this._render();
+  }
+  remove(phoneId){
+    let idFounded = this.find(phoneId);
+    if (idFounded !== false) {
+      this._insideCart.splice(idFounded,1);
+      this._render();
+    }
+  }
+  removeAll(){
+    this._insideCart = [];
     this._render();
   }
 
-  _render() {
+  _render(){
     this._element.innerHTML = `
       <p>Shopping Cart</p>
       <ul>
-        <li>Phone 1</li>
-        <li>Phone 2</li>
-        <li>Phone 3</li>
+         ${ 
+            this._insideCart.map((elem) =>
+            ` <li data-id = "${ elem[0] }">
+                ${ elem[0] }
+                <button data-element="delete-button">-</button> 
+                ${ elem[1] }
+                <button data-element="add-button">+</button> 
+                <button data-element="remove-button">Удалить</button>
+              </li>`
+          ).join('')
+        }
       </ul>
+      <button data-element = "clean-cart">Очистить корзину</button>
     `;
   }
 }
