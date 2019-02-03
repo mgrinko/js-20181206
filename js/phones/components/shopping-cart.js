b7c79b287c2a08b1fc7b4717b0b18380350a0918
Kapush <kapushoniha@gmail.com>
2019-02-03 19:23:19 +0530

    Basket +/-/drop

--- js/phones/components/shopping-cart.js
+++ js/phones/components/shopping-cart.js
@@ -3,38 +3,72 @@ import Component from "./component.js";
 export default class ShoppingCart extends Component {
   constructor({ element }) {
     super({element});
-    this._items = [];
+    this._basket = {};
 
     this.on('click', 'remove-button', (event)=>{
       const itemToDelete = event.target.dataset.itemId;
       this.remove(itemToDelete);
     });
+
+    this.on('click', 'add-button', (event)=>{
+      const itemToAdd = event.target.dataset.itemId;
+      this.add(itemToAdd);
+    });
+
+    this.on('click', 'drop-button', (event)=>{
+      this.drop();
+    });
   }
 
   add(itemId) {
-    this._items.push(itemId);
+    let basketItemCount = this._basket[itemId] || 0;
+    basketItemCount++;
+    this._basket[itemId] = basketItemCount;
     this._render();
   }
 
   remove(itemId) {
-    this._items = this._items.filter(item => item !== itemId);
+    this._basket[itemId]--;
+    if(this._basket[itemId] === 0){
+      delete this._basket[itemId];
+    } 
     this._render();
   }
 
+  drop(){
+    this._basket = {};
+    this._element.innerHTML = '';
+  }
+
   _render() {
+    let basketItem = "";
+    for(let itemId in this._basket){
+      basketItem+=`
+      <li>${ itemId }&nbsp;
+      ${ this._basket[itemId] }
+      <a 
+      data-element="add-button" 
+      data-item-id="${ itemId }"
+      class="cart__remove-button">+
+      </a>
+      <a 
+        data-element="remove-button" 
+        data-item-id="${ itemId }"
+        class="cart__remove-button">-
+      </a>
+    </li>
+      `
+    }
+
     this._element.innerHTML = `
       <p>Shopping Cart</p>
+      <a 
+        data-element="drop-button" 
+        class="cart__remove-button">
+        Очистить
+      </a>
       <ul>
-      ${this._items.map(itemId=>`
-        <li>${itemId}
-          <a 
-            data-element="remove-button" 
-            data-item-id="${ itemId }"
-            class="cart__remove-button">×
-          </a>
-        </li>
-      `).join('')}
-        
+      ${basketItem}
       </ul>
     `;
   }
