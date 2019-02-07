2eefa8ab1b52f3d991b328c50103b29cdad13426
mgrinko <glukym@gmail.com>
2019-02-07 19:38:30 +0200

    Fetch was added

--- js/phones/phone-service.js
+++ js/phones/phone-service.js
@@ -2,16 +2,14 @@
 
 const PhoneService = {
 
-  getAll({ query = '', orderBy = 'age', page = 1, perPage = 10 } = {}) {
+  async getAll({ query = '', orderBy = 'age' }) {
     let url = 'https://mgrinko.github.io/js-20181206/phones/phones.json';
 
-    return this._sendRequest(url)
-      .then((phones) => {
-        const filteredPhones = this._filter(phones, query);
-        const sortedPhones = this._sort(filteredPhones, orderBy);
+    const phones = await this._sendRequest(url);
+    const filteredPhones = this._filter(phones, query);
+    const sortedPhones = this._sort(filteredPhones, orderBy);
 
-        return sortedPhones;
-      });
+    return sortedPhones;
   },
 
   getById(phoneId, callback) {
@@ -20,27 +18,9 @@ const PhoneService = {
     return this._sendRequest(url);
   },
 
-
   _sendRequest(url) {
-    const instructionsFn = (resolve, reject) => {
-      let xhr = new XMLHttpRequest();
-
-      xhr.open('GET', url, true);
-      xhr.send();
-
-      xhr.onload = () => {
-        if (xhr.status !== 200) {
-          reject(`Server error: ${ xhr.status } ${ xhr.statusText }`);
-          return;
-        }
-
-        const data = JSON.parse(xhr.responseText);
-
-        resolve(data);
-      };
-    };
-
-    return new Promise(instructionsFn);
+    return fetch(url)
+      .then(response => response.json())
   },
 
   _filter(phones, query) {
