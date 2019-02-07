944482ef9b9890e5f3d78df23c8894e7846fae55
mgrinko <glukym@gmail.com>
2019-02-07 20:26:22 +0200

    Promise then was implemented

--- js/promise.js
+++ js/promise.js
@@ -1,26 +1,50 @@
 'use strict';
 
+const STATUS_PENDING = 'pending';
+const STATUS_RESOLVED = 'resolved';
+const STATUS_REJECTED = 'rejected';
+
 class MyPromise {
   constructor(instructionsFn) {
-    this._status = 'Waiting';
+    this._status = STATUS_PENDING;
+    this._errorCallbacks = [];
+    this._successCallbacks = [];
 
     instructionsFn(this.resolve.bind(this), this.reject.bind(this));
   }
 
   resolve(data) {
-    if (this._status !== 'Waiting') {
+    if (this._status !== STATUS_PENDING) {
       return;
     }
 
-    this._status = 'Success';
+    this._status = STATUS_RESOLVED;
+
+    for (let callback of this._successCallbacks) {
+      callback(data);
+    }
   }
 
   reject(error) {
-    if (this._status !== 'Waiting') {
+    if (this._status !== STATUS_PENDING) {
       return;
     }
 
-    this._status = 'Error';
+    this._status = STATUS_REJECTED;
+
+    for (let callback of this._errorCallbacks) {
+      callback(error);
+    }
+  }
+
+  then(onSuccess, onError) {
+    if (onSuccess) {
+      this._successCallbacks.push(onSuccess)
+    }
+
+    if (onError) {
+      this._errorCallbacks.push(onSuccess)
+    }
   }
 }
 
