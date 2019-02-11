85f3fcc2f758124354f61bfee8ea2ea090668615
mgrinko <glukym@gmail.com>
2019-02-11 18:58:33 +0200

    Babel loader was added

--- webpack.config.js
+++ webpack.config.js
@@ -6,5 +6,74 @@ module.exports = {
   output: {
     path: path.resolve(__dirname, 'public'),
     filename: 'main.js'
+  },
+
+  devtool: "source-map",
+
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /(node_modules)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
+            plugins: ['@babel/plugin-transform-runtime']
+          }
+        }
+      }
+    ]
   }
 };
+
+// const path = require('path');
+// const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
+//
+// module.exports = {
+//   mode: 'none',
+//   entry: './scripts/app.js',
+//   output: {
+//     path: path.resolve(__dirname, 'public'),
+//     filename: 'build.js'
+//   },
+//
+//   watch: true,
+//   devtool: 'source-map',
+//
+//   module: {
+//     rules: [
+//       {
+//         test: /\.hbs$/,
+//         loader: "handlebars-loader"
+//       },
+//       {
+//         test: /\.css$/,
+//         use: [ 'style-loader', 'css-loader' ]
+//       },
+//       {
+//         test: /\.js$/,
+//         exclude: /(node_modules)/,
+//         use: {
+//           loader: 'babel-loader',
+//           options: {
+//             presets: ['@babel/preset-env'],
+//             plugins: ['@babel/plugin-transform-runtime'],
+//           }
+//         }
+//       }
+//     ]
+//   },
+//
+//   optimization: {
+//     minimize: true,
+//     minimizer: [new UglifyJsPlugin({
+//       sourceMap: true,
+//     })],
+//   },
+//
+//   devServer: {
+//     contentBase: path.join(__dirname, 'public'),
+//     port: 9000
+//   }
+// };
